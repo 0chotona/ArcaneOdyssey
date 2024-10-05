7b3a4ed4e7f5ca03d6a87e2179011f23310ecdff
0chotona <eunjongh97@gmail.com>
2024-10-06 02:08:49 +0900

    241005 / 모모이 기본 스킬 업데이트

--- Assets/Scripts/Attack/Attack_Gun.cs
+++ Assets/Scripts/Attack/Attack_Gun.cs
@@ -7,15 +7,14 @@ public class Attack_Gun : Attack
     [SerializeField] GameObject _damageBoxObj;
     DamageBox_Gun _damageBox;
 
-    LineRenderer _lineRenderer;
     [Header("총알 발사 위치"), SerializeField] Transform _shootTrs;
     [Header("총알 이펙트 발사 위치"), SerializeField] Transform _shootLineTrs;
     [Header("발사 간격"), SerializeField] float _shootGap = 0.15f;
-    [Header("유지 시간"), SerializeField] float _shootDur = 0.05f;
     [Header("발사 이펙트"), SerializeField] ParticleSystem _shootEffect;
     [Header("총알 두께"), SerializeField] float _thickness = 0.1f;
     Vector3 _dir;
-    float _distance = 15f;
+    [Header("총알 거리"), SerializeField] float _distance = 15f;
+    [Header("총알 속도"), SerializeField] float _speed = 10f;
 
     private void OnEnable()
     {
@@ -24,60 +23,18 @@ public class Attack_Gun : Attack
     private void Start()
     {
         StartCoroutine(CRT_Attack());
-        _lineRenderer = GetComponent<LineRenderer>();
-        _lineRenderer.positionCount = 2;
-        _lineRenderer.enabled = false;
-
         _isMaxLevel = false;
         _shootEffect.Stop();
     }
     public override void AttackInteract()
     {
-        Vector3 hitPos = _shootTrs.position + _shootTrs.forward * _distance * _attRange;
-        float radius = _thickness * 0.5f; // 레이저의 반지름 (두께의 절반)
+        Vector3 targetPos = _shootTrs.position + _shootTrs.forward * _distance;
+        GameObject bullet = Instantiate(_damageBoxObj, _shootTrs.position, _shootTrs.rotation);
+        DamageBox_Gun damageBox = bullet.GetComponent<DamageBox_Gun>();
+        damageBox.UpdateDamage(_damage);
+        damageBox.UpdateSpeed(_speed);
+        damageBox.Shot(targetPos);
 
-        /*
-        if (!_isMaxLevel)
-        {
-            RaycastHit hit;
-            if (Physics.SphereCast(_shootTrs.position, radius, _shootTrs.forward, out hit, _distance * _attRange))
-            {
-                if (hit.collider.CompareTag("Enemy"))
-                {
-                    GameObject enemyObject = hit.collider.gameObject;
-                    EnemyHealth enemyHealth = enemyObject.GetComponent<EnemyHealth>();
-                    enemyHealth.LoseDamage(_damage);
-                    hitPos = hit.point;
-                }
-            }
-        }
-        else
-        {
-            RaycastHit[] hits = Physics.SphereCastAll(_shootTrs.position, radius, _shootTrs.forward, _distance * _attRange);
-
-            foreach (RaycastHit hit in hits)
-            {
-                if (hit.collider.CompareTag("Enemy"))
-                {
-                    GameObject enemyObject = hit.collider.gameObject;
-                    EnemyHealth enemyHealth = enemyObject.GetComponent<EnemyHealth>();
-                    enemyHealth.LoseDamage(_damage);
-                }
-            }
-        }
-        */
-        RaycastHit[] hits = Physics.SphereCastAll(_shootTrs.position, radius, _shootTrs.forward, _distance * _attRange);
-
-        foreach (RaycastHit hit in hits)
-        {
-            if (hit.collider.CompareTag("Enemy"))
-            {
-                GameObject enemyObject = hit.collider.gameObject;
-                EnemyHealth enemyHealth = enemyObject.GetComponent<EnemyHealth>();
-                enemyHealth.LoseDamage(_damage);
-            }
-        }
-        PrintLineRenderer(hitPos);
     }
     public void SetMaxLevel() { _isMaxLevel = true; }
     public override void SetSkill(CSkill skill)
@@ -93,14 +50,12 @@ public class Attack_Gun : Attack
         while (true)
         {
 
-            yield return new WaitForSeconds(_coolTime);
+            yield return new WaitForSeconds(_coolTime - _attCount * _shootGap);
             _dir = transform.forward;
             for (int i = 0; i < _attCount; i++)
             {
                 _shootEffect.Play();
                 AttackInteract();
-                yield return new WaitForSeconds(_shootDur);
-                _lineRenderer.enabled = false;
                 yield return new WaitForSeconds(_shootGap);
             }
                 
@@ -108,7 +63,6 @@ public class Attack_Gun : Attack
         }
 
     }
-
     public override void UpdateStat(CStat stat)
     {
         _damage = stat._damage;
@@ -118,53 +72,6 @@ public class Attack_Gun : Attack
         if(_level >= 6)
             _isMaxLevel = true;
     }
-    void PrintLineRenderer(Vector3 hitPos)
-    {
-        _lineRenderer.SetPosition(0, _shootLineTrs.position);
-        _lineRenderer.SetPosition(1, hitPos);
-        _lineRenderer.enabled = true;
-    }
     public override void StartAttack() { return; }
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red; // Gizmos의 색상을 설정
-
-        Vector3 start = _shootTrs.position; // 시작 위치
-        Vector3 direction = _shootTrs.forward; // 레이저 방향
-        float radius = _thickness * 0.5f; // 레이저의 반지름
-        float maxDistance = _distance * _attRange; // 최대 거리
-
-
-        if (!_isMaxLevel)
-        {
-            // Single SphereCast를 시각화
-            RaycastHit hit;
-            if (Physics.SphereCast(start, radius, direction, out hit, maxDistance))
-            {
-                Gizmos.DrawWireSphere(hit.point, radius); // 적과 충돌한 지점에 구를 그리기
-                Gizmos.DrawLine(start, hit.point); // 레이저 경로 그리기
-            }
-            else
-            {
-                // 충돌이 없을 때 최대 거리까지의 궤적을 그리기
-                Gizmos.DrawLine(start, start + direction * maxDistance);
-            }
-        }
-        else
-        {
-            // 여러 개의 SphereCast를 시각화
-            RaycastHit[] hits = Physics.SphereCastAll(start, radius, direction, maxDistance);
-
-            if (hits.Length > 0)
-            {
-                foreach (RaycastHit hit in hits)
-                {
-                    Gizmos.DrawWireSphere(hit.point, radius); // 충돌한 지점마다 구를 그리기
-                }
-            }
-
-            // 충돌이 없더라도 최대 거리까지의 궤적을 그리기
-            Gizmos.DrawLine(start, start + direction * maxDistance);
-        }
-    }
+    
 }
--- Assets/Scripts/Attack/DamageBox_Gun.cs
+++ Assets/Scripts/Attack/DamageBox_Gun.cs
@@ -1,38 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class DamageBox_Gun : MonoBehaviour
 {
     float _damage;
-    float _speed = 2000f;
-    Rigidbody _rigid;
+    public float _speed;
 
-    Vector3 _dir;
 
     Vector3 _boxScale = Vector3.one;
 
-    private void Start()
-    {
-        _rigid = GetComponent<Rigidbody>();
-        Shot();
-    }
-    public void SetDirection(Vector3 dir)
-    {
-        _dir = dir;
-    }
-    void Shot()
-    {
-        transform.rotation = Quaternion.LookRotation(_dir);
-        _rigid.AddForce(_dir * _speed);
-    }
+    GameObject _tmpObj;
+
+    bool _isMaxLevel = false;
+
+    Vector3 _targetPos;
+
     public void UpdateDamage(float damage) { _damage = damage; }
+    public void UpdateSpeed(float speed) { _speed = speed; }
+    public void Shot(Vector3 targetPos)
+    {
+        _tmpObj = gameObject;
+        _targetPos = targetPos;
+        transform.rotation = Quaternion.LookRotation(_targetPos);
+        StartCoroutine(CRT_MoveBullet());
+    }
+    IEnumerator CRT_MoveBullet()
+    {
+        while (Vector3.Distance(transform.position, _targetPos) > 0.05f)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _targetPos, _speed * Time.deltaTime);
+            yield return null;
+        }
+        //Destroy(_tmpObj);
+        
+    }
+    
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Enemy"))
         {
             EnemyHealth enemyHealth = other.GetComponent<EnemyHealth>();
             enemyHealth.LoseDamage(_damage);
+            if (!_isMaxLevel)
+                Destroy(_tmpObj);
         }
     }
 }
